007b6d1aeb22b5704e129171a3b207033e9a7076
Igor <igoroliphirenko@gmail.com>
2019-04-13 12:14:25 +0300

    Это наш первый комит

---
+++ JS Lessons/script.js
@@ -0,0 +1,18 @@
+/*let answer = prompt ("Каково «официальное» название JavaScript?", "");
+    if (answer === "ECMAScript"){
+        alert ("Верно!");
+    } 
+    else if (answer !== "ECMAScript"){
+        alert ("Не знаете? «ECMAScript»!")
+    }
+*/
+let value = prompt ("Enter number", "");
+    if (value > 0){
+        alert ("1");
+    }
+    else if (value < 0 ){
+        alert ("-1");
+    }
+    else if(value == 0){
+        alert("0");
+    }
---
+++ JS Lessons/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let value = prompt ("Enter number", "");
+    if (value > 0){
+        alert ("1");
+    }
+    else (value < 0 ){
+        alert ("-1");
+    }
+    else (value === 0){
+        alert("0");
+    }
